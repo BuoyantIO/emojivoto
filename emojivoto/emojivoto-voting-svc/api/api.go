415bbc93937b670024675b18da0cae245679a9e7
Kevin Lingerfelt <kl@buoyant.io>
2017-12-04 13:52:47 -0800

    Create separate vote endpoint for each emoji (#14)

--- emojivoto/emojivoto-voting-svc/api/api.go
+++ emojivoto/emojivoto-voting-svc/api/api.go
@@ -13,13 +13,409 @@ type PollServiceServer struct {
 	poll voting.Poll
 }
 
-func (pS *PollServiceServer) Vote(context context.Context, req *pb.VoteRequest) (*pb.VoteResponse, error) {
-	sc := req.Shortcode
-	if sc == ":shit:" || sc == ":poop:" || sc == ":hankey:" {
-		return nil, fmt.Errorf("ERROR")
-	}
-	err := pS.poll.Vote(sc)
+func (pS *PollServiceServer) vote(shortcode string) (*pb.VoteResponse, error) {
+	err := pS.poll.Vote(shortcode)
 	return &pb.VoteResponse{}, err
+}
+
+func (pS *PollServiceServer) VotePoop(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return nil, fmt.Errorf("ERROR")
+}
+
+func (pS *PollServiceServer) VoteJoy(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":joy:")
+}
+
+func (pS *PollServiceServer) VoteSunglasses(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":sunglasses:")
+}
+
+func (pS *PollServiceServer) VoteRelaxed(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":relaxed:")
+}
+
+func (pS *PollServiceServer) VoteStuckOutTongueWinkingEye(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":stuck_out_tongue_winking_eye:")
+}
+
+func (pS *PollServiceServer) VoteMoneyMouthFace(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":money_mouth_face:")
+}
+
+func (pS *PollServiceServer) VoteFlushed(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":flushed:")
+}
+
+func (pS *PollServiceServer) VoteMask(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":mask:")
+}
+
+func (pS *PollServiceServer) VoteNerdFace(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":nerd_face:")
+}
+
+func (pS *PollServiceServer) VoteGhost(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":ghost:")
+}
+
+func (pS *PollServiceServer) VoteSkullAndCrossbones(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":skull_and_crossbones:")
+}
+
+func (pS *PollServiceServer) VoteHeartEyesCat(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":heart_eyes_cat:")
+}
+
+func (pS *PollServiceServer) VoteHearNoEvil(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":hear_no_evil:")
+}
+
+func (pS *PollServiceServer) VoteSeeNoEvil(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":see_no_evil:")
+}
+
+func (pS *PollServiceServer) VoteSpeakNoEvil(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":speak_no_evil:")
+}
+
+func (pS *PollServiceServer) VoteBoy(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":boy:")
+}
+
+func (pS *PollServiceServer) VoteGirl(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":girl:")
+}
+
+func (pS *PollServiceServer) VoteMan(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":man:")
+}
+
+func (pS *PollServiceServer) VoteWoman(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":woman:")
+}
+
+func (pS *PollServiceServer) VoteOlderMan(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":older_man:")
+}
+
+func (pS *PollServiceServer) VotePoliceman(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":policeman:")
+}
+
+func (pS *PollServiceServer) VoteGuardsman(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":guardsman:")
+}
+
+func (pS *PollServiceServer) VoteConstructionWorkerMan(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":construction_worker_man:")
+}
+
+func (pS *PollServiceServer) VotePrince(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":prince:")
+}
+
+func (pS *PollServiceServer) VotePrincess(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":princess:")
+}
+
+func (pS *PollServiceServer) VoteManInTuxedo(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":man_in_tuxedo:")
+}
+
+func (pS *PollServiceServer) VoteBrideWithVeil(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":bride_with_veil:")
+}
+
+func (pS *PollServiceServer) VoteMrsClaus(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":mrs_claus:")
+}
+
+func (pS *PollServiceServer) VoteSanta(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":santa:")
+}
+
+func (pS *PollServiceServer) VoteTurkey(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":turkey:")
+}
+
+func (pS *PollServiceServer) VoteRabbit(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":rabbit:")
+}
+
+func (pS *PollServiceServer) VoteNoGoodWoman(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":no_good_woman:")
+}
+
+func (pS *PollServiceServer) VoteOkWoman(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":ok_woman:")
+}
+
+func (pS *PollServiceServer) VoteRaisingHandWoman(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":raising_hand_woman:")
+}
+
+func (pS *PollServiceServer) VoteBowingMan(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":bowing_man:")
+}
+
+func (pS *PollServiceServer) VoteManFacepalming(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":man_facepalming:")
+}
+
+func (pS *PollServiceServer) VoteWomanShrugging(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":woman_shrugging:")
+}
+
+func (pS *PollServiceServer) VoteMassageWoman(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":massage_woman:")
+}
+
+func (pS *PollServiceServer) VoteWalkingMan(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":walking_man:")
+}
+
+func (pS *PollServiceServer) VoteRunningMan(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":running_man:")
+}
+
+func (pS *PollServiceServer) VoteDancer(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":dancer:")
+}
+
+func (pS *PollServiceServer) VoteManDancing(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":man_dancing:")
+}
+
+func (pS *PollServiceServer) VoteDancingWomen(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":dancing_women:")
+}
+
+func (pS *PollServiceServer) VoteRainbow(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":rainbow:")
+}
+
+func (pS *PollServiceServer) VoteSkier(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":skier:")
+}
+
+func (pS *PollServiceServer) VoteGolfingMan(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":golfing_man:")
+}
+
+func (pS *PollServiceServer) VoteSurfingMan(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":surfing_man:")
+}
+
+func (pS *PollServiceServer) VoteBasketballMan(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":basketball_man:")
+}
+
+func (pS *PollServiceServer) VoteBikingMan(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":biking_man:")
+}
+
+func (pS *PollServiceServer) VotePointUp2(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":point_up_2:")
+}
+
+func (pS *PollServiceServer) VoteVulcanSalute(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":vulcan_salute:")
+}
+
+func (pS *PollServiceServer) VoteMetal(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":metal:")
+}
+
+func (pS *PollServiceServer) VoteCallMeHand(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":call_me_hand:")
+}
+
+func (pS *PollServiceServer) VoteThumbsup(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":thumbsup:")
+}
+
+func (pS *PollServiceServer) VoteWave(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":wave:")
+}
+
+func (pS *PollServiceServer) VoteClap(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":clap:")
+}
+
+func (pS *PollServiceServer) VoteRaisedHands(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":raised_hands:")
+}
+
+func (pS *PollServiceServer) VotePray(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":pray:")
+}
+
+func (pS *PollServiceServer) VoteDog(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":dog:")
+}
+
+func (pS *PollServiceServer) VoteCat2(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":cat2:")
+}
+
+func (pS *PollServiceServer) VotePig(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":pig:")
+}
+
+func (pS *PollServiceServer) VoteHatchingChick(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":hatching_chick:")
+}
+
+func (pS *PollServiceServer) VoteSnail(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":snail:")
+}
+
+func (pS *PollServiceServer) VoteBacon(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":bacon:")
+}
+
+func (pS *PollServiceServer) VotePizza(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":pizza:")
+}
+
+func (pS *PollServiceServer) VoteTaco(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":taco:")
+}
+
+func (pS *PollServiceServer) VoteBurrito(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":burrito:")
+}
+
+func (pS *PollServiceServer) VoteRamen(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":ramen:")
+}
+
+func (pS *PollServiceServer) VoteDoughnut(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":doughnut:")
+}
+
+func (pS *PollServiceServer) VoteChampagne(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":champagne:")
+}
+
+func (pS *PollServiceServer) VoteTropicalDrink(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":tropical_drink:")
+}
+
+func (pS *PollServiceServer) VoteBeer(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":beer:")
+}
+
+func (pS *PollServiceServer) VoteTumblerGlass(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":tumbler_glass:")
+}
+
+func (pS *PollServiceServer) VoteWorldMap(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":world_map:")
+}
+
+func (pS *PollServiceServer) VoteBeachUmbrella(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":beach_umbrella:")
+}
+
+func (pS *PollServiceServer) VoteMountainSnow(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":mountain_snow:")
+}
+
+func (pS *PollServiceServer) VoteCamping(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":camping:")
+}
+
+func (pS *PollServiceServer) VoteSteamLocomotive(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":steam_locomotive:")
+}
+
+func (pS *PollServiceServer) VoteFlightDeparture(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":flight_departure:")
+}
+
+func (pS *PollServiceServer) VoteRocket(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":rocket:")
+}
+
+func (pS *PollServiceServer) VoteStar2(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":star2:")
+}
+
+func (pS *PollServiceServer) VoteSunBehindSmallCloud(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":sun_behind_small_cloud:")
+}
+
+func (pS *PollServiceServer) VoteCloudWithRain(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":cloud_with_rain:")
+}
+
+func (pS *PollServiceServer) VoteFire(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":fire:")
+}
+
+func (pS *PollServiceServer) VoteJackOLantern(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":jack_o_lantern:")
+}
+
+func (pS *PollServiceServer) VoteBalloon(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":balloon:")
+}
+
+func (pS *PollServiceServer) VoteTada(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":tada:")
+}
+
+func (pS *PollServiceServer) VoteTrophy(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":trophy:")
+}
+
+func (pS *PollServiceServer) VoteIphone(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":iphone:")
+}
+
+func (pS *PollServiceServer) VotePager(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":pager:")
+}
+
+func (pS *PollServiceServer) VoteFax(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":fax:")
+}
+
+func (pS *PollServiceServer) VoteBulb(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":bulb:")
+}
+
+func (pS *PollServiceServer) VoteMoneyWithWings(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":money_with_wings:")
+}
+
+func (pS *PollServiceServer) VoteCrystalBall(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":crystal_ball:")
+}
+
+func (pS *PollServiceServer) VoteUnderage(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":underage:")
+}
+
+func (pS *PollServiceServer) VoteInterrobang(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":interrobang:")
+}
+
+func (pS *PollServiceServer) Vote100(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":100:")
+}
+
+func (pS *PollServiceServer) VoteCheckeredFlag(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":checkered_flag:")
+}
+
+func (pS *PollServiceServer) VoteCrossedSwords(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":crossed_swords:")
+}
+
+func (pS *PollServiceServer) VoteFloppyDisk(_ context.Context, _ *pb.VoteRequest) (*pb.VoteResponse, error) {
+	return pS.vote(":floppy_disk:")
 }
 
 func (pS *PollServiceServer) Results(context.Context, *pb.ResultsRequest) (*pb.ResultsResponse, error) {
--- emojivoto/emojivoto-web/web/web.go
+++ emojivoto/emojivoto-web/web/web.go
@@ -93,17 +93,215 @@ func (app *WebApp) voteEmojiHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	chosenEmoji := response.Emoji
-	if chosenEmoji == nil {
+	if response.Emoji == nil {
 		err = errors.New(fmt.Sprintf("Choosen emoji shortcode [%s] doesnt exist", emojiShortcode))
 		writeError(err, w, r, http.StatusBadRequest)
 		return
 	}
 
-	voteRequest := &pb.VoteRequest{
-		Shortcode: chosenEmoji.Shortcode,
+	voteRequest := &pb.VoteRequest{}
+	switch emojiShortcode {
+	case ":poop:":
+		_, err = app.votingServiceClient.VotePoop(r.Context(), voteRequest)
+	case ":joy:":
+		_, err = app.votingServiceClient.VoteJoy(r.Context(), voteRequest)
+	case ":sunglasses:":
+		_, err = app.votingServiceClient.VoteSunglasses(r.Context(), voteRequest)
+	case ":relaxed:":
+		_, err = app.votingServiceClient.VoteRelaxed(r.Context(), voteRequest)
+	case ":stuck_out_tongue_winking_eye:":
+		_, err = app.votingServiceClient.VoteStuckOutTongueWinkingEye(r.Context(), voteRequest)
+	case ":money_mouth_face:":
+		_, err = app.votingServiceClient.VoteMoneyMouthFace(r.Context(), voteRequest)
+	case ":flushed:":
+		_, err = app.votingServiceClient.VoteFlushed(r.Context(), voteRequest)
+	case ":mask:":
+		_, err = app.votingServiceClient.VoteMask(r.Context(), voteRequest)
+	case ":nerd_face:":
+		_, err = app.votingServiceClient.VoteNerdFace(r.Context(), voteRequest)
+	case ":ghost:":
+		_, err = app.votingServiceClient.VoteGhost(r.Context(), voteRequest)
+	case ":skull_and_crossbones:":
+		_, err = app.votingServiceClient.VoteSkullAndCrossbones(r.Context(), voteRequest)
+	case ":heart_eyes_cat:":
+		_, err = app.votingServiceClient.VoteHeartEyesCat(r.Context(), voteRequest)
+	case ":hear_no_evil:":
+		_, err = app.votingServiceClient.VoteHearNoEvil(r.Context(), voteRequest)
+	case ":see_no_evil:":
+		_, err = app.votingServiceClient.VoteSeeNoEvil(r.Context(), voteRequest)
+	case ":speak_no_evil:":
+		_, err = app.votingServiceClient.VoteSpeakNoEvil(r.Context(), voteRequest)
+	case ":boy:":
+		_, err = app.votingServiceClient.VoteBoy(r.Context(), voteRequest)
+	case ":girl:":
+		_, err = app.votingServiceClient.VoteGirl(r.Context(), voteRequest)
+	case ":man:":
+		_, err = app.votingServiceClient.VoteMan(r.Context(), voteRequest)
+	case ":woman:":
+		_, err = app.votingServiceClient.VoteWoman(r.Context(), voteRequest)
+	case ":older_man:":
+		_, err = app.votingServiceClient.VoteOlderMan(r.Context(), voteRequest)
+	case ":policeman:":
+		_, err = app.votingServiceClient.VotePoliceman(r.Context(), voteRequest)
+	case ":guardsman:":
+		_, err = app.votingServiceClient.VoteGuardsman(r.Context(), voteRequest)
+	case ":construction_worker_man:":
+		_, err = app.votingServiceClient.VoteConstructionWorkerMan(r.Context(), voteRequest)
+	case ":prince:":
+		_, err = app.votingServiceClient.VotePrince(r.Context(), voteRequest)
+	case ":princess:":
+		_, err = app.votingServiceClient.VotePrincess(r.Context(), voteRequest)
+	case ":man_in_tuxedo:":
+		_, err = app.votingServiceClient.VoteManInTuxedo(r.Context(), voteRequest)
+	case ":bride_with_veil:":
+		_, err = app.votingServiceClient.VoteBrideWithVeil(r.Context(), voteRequest)
+	case ":mrs_claus:":
+		_, err = app.votingServiceClient.VoteMrsClaus(r.Context(), voteRequest)
+	case ":santa:":
+		_, err = app.votingServiceClient.VoteSanta(r.Context(), voteRequest)
+	case ":turkey:":
+		_, err = app.votingServiceClient.VoteTurkey(r.Context(), voteRequest)
+	case ":rabbit:":
+		_, err = app.votingServiceClient.VoteRabbit(r.Context(), voteRequest)
+	case ":no_good_woman:":
+		_, err = app.votingServiceClient.VoteNoGoodWoman(r.Context(), voteRequest)
+	case ":ok_woman:":
+		_, err = app.votingServiceClient.VoteOkWoman(r.Context(), voteRequest)
+	case ":raising_hand_woman:":
+		_, err = app.votingServiceClient.VoteRaisingHandWoman(r.Context(), voteRequest)
+	case ":bowing_man:":
+		_, err = app.votingServiceClient.VoteBowingMan(r.Context(), voteRequest)
+	case ":man_facepalming:":
+		_, err = app.votingServiceClient.VoteManFacepalming(r.Context(), voteRequest)
+	case ":woman_shrugging:":
+		_, err = app.votingServiceClient.VoteWomanShrugging(r.Context(), voteRequest)
+	case ":massage_woman:":
+		_, err = app.votingServiceClient.VoteMassageWoman(r.Context(), voteRequest)
+	case ":walking_man:":
+		_, err = app.votingServiceClient.VoteWalkingMan(r.Context(), voteRequest)
+	case ":running_man:":
+		_, err = app.votingServiceClient.VoteRunningMan(r.Context(), voteRequest)
+	case ":dancer:":
+		_, err = app.votingServiceClient.VoteDancer(r.Context(), voteRequest)
+	case ":man_dancing:":
+		_, err = app.votingServiceClient.VoteManDancing(r.Context(), voteRequest)
+	case ":dancing_women:":
+		_, err = app.votingServiceClient.VoteDancingWomen(r.Context(), voteRequest)
+	case ":rainbow:":
+		_, err = app.votingServiceClient.VoteRainbow(r.Context(), voteRequest)
+	case ":skier:":
+		_, err = app.votingServiceClient.VoteSkier(r.Context(), voteRequest)
+	case ":golfing_man:":
+		_, err = app.votingServiceClient.VoteGolfingMan(r.Context(), voteRequest)
+	case ":surfing_man:":
+		_, err = app.votingServiceClient.VoteSurfingMan(r.Context(), voteRequest)
+	case ":basketball_man:":
+		_, err = app.votingServiceClient.VoteBasketballMan(r.Context(), voteRequest)
+	case ":biking_man:":
+		_, err = app.votingServiceClient.VoteBikingMan(r.Context(), voteRequest)
+	case ":point_up_2:":
+		_, err = app.votingServiceClient.VotePointUp2(r.Context(), voteRequest)
+	case ":vulcan_salute:":
+		_, err = app.votingServiceClient.VoteVulcanSalute(r.Context(), voteRequest)
+	case ":metal:":
+		_, err = app.votingServiceClient.VoteMetal(r.Context(), voteRequest)
+	case ":call_me_hand:":
+		_, err = app.votingServiceClient.VoteCallMeHand(r.Context(), voteRequest)
+	case ":thumbsup:":
+		_, err = app.votingServiceClient.VoteThumbsup(r.Context(), voteRequest)
+	case ":wave:":
+		_, err = app.votingServiceClient.VoteWave(r.Context(), voteRequest)
+	case ":clap:":
+		_, err = app.votingServiceClient.VoteClap(r.Context(), voteRequest)
+	case ":raised_hands:":
+		_, err = app.votingServiceClient.VoteRaisedHands(r.Context(), voteRequest)
+	case ":pray:":
+		_, err = app.votingServiceClient.VotePray(r.Context(), voteRequest)
+	case ":dog:":
+		_, err = app.votingServiceClient.VoteDog(r.Context(), voteRequest)
+	case ":cat2:":
+		_, err = app.votingServiceClient.VoteCat2(r.Context(), voteRequest)
+	case ":pig:":
+		_, err = app.votingServiceClient.VotePig(r.Context(), voteRequest)
+	case ":hatching_chick:":
+		_, err = app.votingServiceClient.VoteHatchingChick(r.Context(), voteRequest)
+	case ":snail:":
+		_, err = app.votingServiceClient.VoteSnail(r.Context(), voteRequest)
+	case ":bacon:":
+		_, err = app.votingServiceClient.VoteBacon(r.Context(), voteRequest)
+	case ":pizza:":
+		_, err = app.votingServiceClient.VotePizza(r.Context(), voteRequest)
+	case ":taco:":
+		_, err = app.votingServiceClient.VoteTaco(r.Context(), voteRequest)
+	case ":burrito:":
+		_, err = app.votingServiceClient.VoteBurrito(r.Context(), voteRequest)
+	case ":ramen:":
+		_, err = app.votingServiceClient.VoteRamen(r.Context(), voteRequest)
+	case ":doughnut:":
+		_, err = app.votingServiceClient.VoteDoughnut(r.Context(), voteRequest)
+	case ":champagne:":
+		_, err = app.votingServiceClient.VoteChampagne(r.Context(), voteRequest)
+	case ":tropical_drink:":
+		_, err = app.votingServiceClient.VoteTropicalDrink(r.Context(), voteRequest)
+	case ":beer:":
+		_, err = app.votingServiceClient.VoteBeer(r.Context(), voteRequest)
+	case ":tumbler_glass:":
+		_, err = app.votingServiceClient.VoteTumblerGlass(r.Context(), voteRequest)
+	case ":world_map:":
+		_, err = app.votingServiceClient.VoteWorldMap(r.Context(), voteRequest)
+	case ":beach_umbrella:":
+		_, err = app.votingServiceClient.VoteBeachUmbrella(r.Context(), voteRequest)
+	case ":mountain_snow:":
+		_, err = app.votingServiceClient.VoteMountainSnow(r.Context(), voteRequest)
+	case ":camping:":
+		_, err = app.votingServiceClient.VoteCamping(r.Context(), voteRequest)
+	case ":steam_locomotive:":
+		_, err = app.votingServiceClient.VoteSteamLocomotive(r.Context(), voteRequest)
+	case ":flight_departure:":
+		_, err = app.votingServiceClient.VoteFlightDeparture(r.Context(), voteRequest)
+	case ":rocket:":
+		_, err = app.votingServiceClient.VoteRocket(r.Context(), voteRequest)
+	case ":star2:":
+		_, err = app.votingServiceClient.VoteStar2(r.Context(), voteRequest)
+	case ":sun_behind_small_cloud:":
+		_, err = app.votingServiceClient.VoteSunBehindSmallCloud(r.Context(), voteRequest)
+	case ":cloud_with_rain:":
+		_, err = app.votingServiceClient.VoteCloudWithRain(r.Context(), voteRequest)
+	case ":fire:":
+		_, err = app.votingServiceClient.VoteFire(r.Context(), voteRequest)
+	case ":jack_o_lantern:":
+		_, err = app.votingServiceClient.VoteJackOLantern(r.Context(), voteRequest)
+	case ":balloon:":
+		_, err = app.votingServiceClient.VoteBalloon(r.Context(), voteRequest)
+	case ":tada:":
+		_, err = app.votingServiceClient.VoteTada(r.Context(), voteRequest)
+	case ":trophy:":
+		_, err = app.votingServiceClient.VoteTrophy(r.Context(), voteRequest)
+	case ":iphone:":
+		_, err = app.votingServiceClient.VoteIphone(r.Context(), voteRequest)
+	case ":pager:":
+		_, err = app.votingServiceClient.VotePager(r.Context(), voteRequest)
+	case ":fax:":
+		_, err = app.votingServiceClient.VoteFax(r.Context(), voteRequest)
+	case ":bulb:":
+		_, err = app.votingServiceClient.VoteBulb(r.Context(), voteRequest)
+	case ":money_with_wings:":
+		_, err = app.votingServiceClient.VoteMoneyWithWings(r.Context(), voteRequest)
+	case ":crystal_ball:":
+		_, err = app.votingServiceClient.VoteCrystalBall(r.Context(), voteRequest)
+	case ":underage:":
+		_, err = app.votingServiceClient.VoteUnderage(r.Context(), voteRequest)
+	case ":interrobang:":
+		_, err = app.votingServiceClient.VoteInterrobang(r.Context(), voteRequest)
+	case ":100:":
+		_, err = app.votingServiceClient.Vote100(r.Context(), voteRequest)
+	case ":checkered_flag:":
+		_, err = app.votingServiceClient.VoteCheckeredFlag(r.Context(), voteRequest)
+	case ":crossed_swords:":
+		_, err = app.votingServiceClient.VoteCrossedSwords(r.Context(), voteRequest)
+	case ":floppy_disk:":
+		_, err = app.votingServiceClient.VoteFloppyDisk(r.Context(), voteRequest)
 	}
-	_, err = app.votingServiceClient.Vote(r.Context(), voteRequest)
 	if err != nil {
 		writeError(err, w, r, http.StatusInternalServerError)
 		return
