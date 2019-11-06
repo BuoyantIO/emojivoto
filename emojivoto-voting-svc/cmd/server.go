47a026c2e4085f4e536c2735f3ff3788b0870072
grampelberg <thomas@saunter.org>
2019-11-06 13:35:33 -0800

    Add tracing to all components, off by default

--- emojivoto-voting-svc/cmd/server.go
+++ emojivoto-voting-svc/cmd/server.go
@@ -5,14 +5,19 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 
 	"github.com/buoyantio/emojivoto/emojivoto-voting-svc/api"
 	"github.com/buoyantio/emojivoto/emojivoto-voting-svc/voting"
 	"google.golang.org/grpc"
+	"contrib.go.opencensus.io/exporter/ocagent"
+	"go.opencensus.io/plugin/ocgrpc"
+	"go.opencensus.io/trace"
 )
 
 var (
 	grpcPort = os.Getenv("GRPC_PORT")
+	ocagentHost = os.Getenv("OC_AGENT_HOST")
 )
 
 func main() {
@@ -21,6 +26,16 @@ func main() {
 		log.Fatalf("GRPC_PORT (currently [%s]) environment variable must me set to run the server.", grpcPort)
 	}
 
+	oce, err := ocagent.NewExporter(
+		ocagent.WithInsecure(),
+		ocagent.WithReconnectionPeriod(5 * time.Second),
+		ocagent.WithAddress(ocagentHost),
+		ocagent.WithServiceName("voting"))
+	if err != nil {
+		log.Fatalf("Failed to create ocagent-exporter: %v", err)
+	}
+	trace.RegisterExporter(oce)
+
 	poll := voting.NewPoll()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", grpcPort))
@@ -28,7 +43,7 @@ func main() {
 		panic(err)
 	}
 
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.StatsHandler(&ocgrpc.ServerHandler{}))
 	api.NewGrpServer(grpcServer, poll)
 	log.Printf("Starting grpc server on GRPC_PORT=[%s]", grpcPort)
 	grpcServer.Serve(lis)
--- go.mod
+++ go.mod
@@ -3,10 +3,8 @@ module github.com/buoyantio/emojivoto
 go 1.13
 
 require (
+	contrib.go.opencensus.io/exporter/ocagent v0.6.0
 	github.com/golang/protobuf v1.3.2
-	golang.org/x/net v0.0.0-20190110200230-915654e7eabc
-	golang.org/x/sys v0.0.0-20190116161447-11f53e031339
-	golang.org/x/text v0.3.0
-	google.golang.org/genproto v0.0.0-20190111180523-db91494dd46c
-	google.golang.org/grpc v1.18.0
+	go.opencensus.io v0.22.1
+	google.golang.org/grpc v1.22.0
 )
