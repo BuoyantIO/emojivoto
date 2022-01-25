a1235b00ebb43a80664ac3985a6d9928fc9ec177
Philippe Granger <pgranger@datawire.io>
2022-01-25 16:30:45 -0500

    🔨 adapt to run with linux or mac

--- emojivoto-web-app/js/setup_dev_env.sh
+++ emojivoto-web-app/js/setup_dev_env.sh
@@ -2,6 +2,60 @@
 
 # global vars
 CONTAINER_ID=''
+OS=''
+ARCH=''
+MOUNT_VOLUME=''
+
+has_cli() {
+
+    hasCurl=$(which curl)
+    if [ "$?" = "1" ]; then
+        echo "You need curl to use this script."
+        exit 1
+    fi
+    hasKubectl=$(which kubectl)
+    if [ "$?" = "1" ]; then
+        echo "You need kubectl to use this script. https://kubernetes.io/docs/tasks/tools/#kubectl"
+        exit 1
+    fi
+}
+
+set_os_arch() {
+    uname=$(uname)
+
+    case $uname in
+        "Darwin")
+            OS="darwin"
+            MOUNT_VOLUME_LOCAL=~/Library/Application\ Support
+            ;;
+        "Linux")
+            OS="linux"
+            MOUNT_VOLUME_LOCAL=$(if [[ "$XDG_CONFIG_HOME" ]]; then echo "$XDG_CONFIG_HOME"; else echo "$HOME/.config"; fi)
+            ;;
+        *)
+            fatal "Unsupported os $uname"
+    esac
+
+    if [ -z "$ARCH" ]; then
+        ARCH=$(uname -m)
+    fi
+    case $ARCH in
+        amd64)
+            ARCH=amd64
+            ;;
+        x86_64)
+            ARCH=amd64
+            ;;
+        arm64)
+            ARCH=arm64
+            ;;
+        aarch64)
+            ARCH=arm64
+            ;;
+        *)
+            fatal "Unsupported architecture $ARCH"
+    esac
+}
 
 check_init_config() {
     if [[ -z "${AMBASSADOR_API_KEY}" ]]; then
@@ -25,7 +79,7 @@ run_dev_container() {
     fi
 
     # run the dev container, exposing 8081 gRPC port and volume mounting code directory
-    CONTAINER_ID=$(docker run -d -p8083:8083 -p8080:8080 --name ambassador-demo --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun --pull always --rm -it -e AMBASSADOR_API_KEY=$AMBASSADOR_API_KEY  -v ~/Library/Application\ Support:/root/.host_config  -v $(pwd):/opt/emojivoto/emojivoto-web-app/js datawire/emojivoto-node-and-go-demo )
+    CONTAINER_ID=$(docker run -d -p8083:8083 -p8080:8080 --name ambassador-demo --cap-add=NET_ADMIN --device /dev/net/tun:/dev/net/tun --pull always --rm -it -e AMBASSADOR_API_KEY=$AMBASSADOR_API_KEY  -v $MOUNT_VOLUME_LOCAL:/root/.host_config  -v $(pwd):/opt/emojivoto/emojivoto-web-app/js datawire/emojivoto-node-and-go-demo )
 }
 
 connect_to_k8s() {
@@ -43,9 +97,10 @@ connect_to_k8s() {
 
 install_telepresence() {
     echo 'Configuring Telepresence'
-    if [ ! command -v telepresence &> /dev/null ];  then
+    hasTelepresence=$(which telepresence)
+    if [ "$?" = "1" ]; then
         echo "Installing Telepresence"
-        sudo curl -fL https://app.getambassador.io/download/tel2/darwin/amd64/latest/telepresence -o /usr/local/bin/telepresence
+        sudo curl -fL https://app.getambassador.io/download/tel2/${OS}/${ARCH}/latest/telepresence -o /usr/local/bin/telepresence
         sudo chmod a+x /usr/local/bin/telepresence
     else
         echo "Telepresence already installed"
@@ -55,15 +110,21 @@ install_telepresence() {
 connect_local_dev_env_to_remote() {
     export KUBECONFIG=./emojivoto_k8s_context.yaml
     echo 'Connecting local dev env to remote K8s cluster'
-    telepresence login
+    telepresence login --apikey=$AMBASSADOR_API_KEY
     telepresence intercept web-app-57bc7c4959 --service web-app --port 8083:80 --ingress-port 80 --ingress-host ambassador.ambassador --ingress-l5 ambassador.ambassador
+    telOut=$?
+    if [ $telOut != 0 ]; then
+        exit $telOut
+    fi
 }
 
 open_editor() {
     echo 'Opening editor'
+    # let the user see the output before opening the editor
+    sleep 2
 
     # replace this line with your editor of choice, e.g. VS code, Intelli J
-    code .
+    $EDITOR .
 }
 
 display_instructions_to_user () {
@@ -74,6 +135,8 @@ display_instructions_to_user () {
     echo 'export KUBECONFIG=./emojivoto_k8s_context.yaml'
 }
 
+has_cli
+set_os_arch
 check_init_config
 run_dev_container
 connect_to_k8s
