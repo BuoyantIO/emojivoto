c31b6a0c8b32ccf1396b75debeb79919a71a866d
Kévin Lambert <kevin.lambert.ca@gmail.com>
2023-06-12 12:08:34 -0400

    Upgrade to 2.14.0-rc.11

--- emojivoto-web-app/js/setup_dev_env.sh
+++ emojivoto-web-app/js/setup_dev_env.sh
@@ -177,7 +177,7 @@ install_upgrade_telepresence() {
         fi
     fi    
     if [ $install_telepresence = true ]; then
-        sudo curl -fL https://app.getambassador.io/download/tel2/${OS}/${ARCH}/2.13.3/telepresence -o /usr/local/bin/telepresence
+        sudo curl -fL https://app.getambassador.io/download/tel2/${OS}/${ARCH}/2.14.0-rc.11/telepresence -o /usr/local/bin/telepresence
         sudo chmod a+x /usr/local/bin/telepresence
         send_telemetry "telepresenceInstalled"
     fi
